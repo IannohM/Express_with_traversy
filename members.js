ba63995d256721a94f6fbf81f74471092d273eaf
IannohM <mutukuianmusyoka@duck.com>
2026-01-09 13:26:34 +0300

    created the user array

---
+++ members.js
@@ -0,0 +1,24 @@
+const users = [
+    { name: 'Sophia Rodriguez', age: 27, gender: 'female' },
+    { name: 'Liam Thompson', age: 32, gender: 'male' },
+    { name: 'Olivia White', age: 25, gender: 'female' },
+    { name: 'Noah Harris', age: 41, gender: 'male' },
+    { name: 'Emma Lewis', age: 30, gender: 'female' },
+    { name: 'Mason Walker', age: 36, gender: 'male' },
+    { name: 'Ava Hall', age: 23, gender: 'female' },
+    { name: 'Lucas Allen', age: 38, gender: 'male' },
+    { name: 'Isabella Young', age: 29, gender: 'female' },
+    { name: 'James King', age: 44, gender: 'male' },
+    { name: 'Mia Wright', age: 26, gender: 'female' },
+    { name: 'Benjamin Scott', age: 33, gender: 'male' },
+    { name: 'Charlotte Green', age: 31, gender: 'female' },
+    { name: 'Henry Adams', age: 40, gender: 'male' },
+    { name: 'Amelia Baker', age: 28, gender: 'female' },
+    { name: 'Alexander Gonzalez', age: 35, gender: 'male' },
+    { name: 'Harper Nelson', age: 24, gender: 'female' },
+    { name: 'Daniel Carter', age: 42, gender: 'male' },
+    { name: 'Evelyn Mitchell', age: 27, gender: 'female' },
+    { name: 'William Perez', age: 39, gender: 'male' },
+];
+
+module.exports = users
