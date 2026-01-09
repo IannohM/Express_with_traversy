610dedb02671992e083d4ddd575315b97cbc424a
IannohM <mutukuianmusyoka@duck.com>
2026-01-09 12:43:22 +0300

    added routes to open the specified folders

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ dotenv.config('./.env');
 
 const app = express();
 
+//& Set A static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+    res.sendFile(path.join(__dirname, 'public', 'pages', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'pages', 'about.html'));
+});
+
+app.get('/*notfound', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'pages', '404.html'));
+});
 
 const PORT = process.env.PORT || 4000;
 
