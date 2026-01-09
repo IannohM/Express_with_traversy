0d489402c342581763626b7edf0365fb47a64702
IannohM <mutukuianmusyoka@duck.com>
2026-01-09 13:22:30 +0300

    Moved the logger middleware to the logger.js in the middleware folder

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
-dotenv.config('./.env');
+dotenv.config('./config.env');
+
+const users = require('./members');
+const Logger = require('./middleware/logger')
 
 const app = express();
+
+
+//& Init Middleware
+// app.use(Logger);
 
 //& Set A static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//& Welcome Page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'pages', 'index.html'));
 });
 
+//& About Page
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'pages', 'about.html'));
 });
 
+//& get all users
+app.get('/api/members', (req, res) => {
+    res.json(users);
+});
+
+//& Page Not Found
 app.get('/*notfound', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'pages', '404.html'));
 });
